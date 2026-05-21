54cf3e78d1ffefcb9537b84ce5c2ac7c7926e425
Dinj-DJ <dylanjager78@gmail.com>
2026-05-21 17:16:52 +1000

    Dark theme

--- quartz.config.ts
+++ quartz.config.ts
@@ -42,3 +42,53 @@ const config: QuartzConfig = {
           light: "#1e1e2e",
           lightgray: "#313244",
           gray: "#6c7086",
+          darkgray: "#cdd6f4",
+          dark: "#cdd6f4",
+          secondary: "#89b4fa",
+          tertiary: "#cba6f7",
+          highlight: "rgba(137, 180, 250, 0.1)",
+          textHighlight: "#f9e2af",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "git", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+      Plugin.CustomOgImages(),
+    ],
+  },
+}
+export default config
